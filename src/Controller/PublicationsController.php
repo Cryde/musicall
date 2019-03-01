16f392e23460ca8014d3806ec0b1c33fba130f0c
Jérémy <t.jeremy.0@gmail.com>
2019-03-01 14:59:21 +0100

    feat: implement basic publication system

--- src/Controller/PublicationsController.php
+++ src/Controller/PublicationsController.php
@@ -2,18 +2,60 @@
 
 namespace App\Controller;
 
+use App\Entity\Publication;
+use App\Form\PublicationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PublicationsController extends AbstractController
 {
     /**
-     * @Route("/publications/", name="app_publications")
+     * @Route("/publications/", name="publications_index")
      *
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      */
-    public function indexAction()
+    public function index()
     {
         return $this->render('publications/index.html.twig');
+    }
+
+
+    /**
+     * @Route("/publications/add", name="publications_add")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function add(Request $request)
+    {
+        $publication = (new Publication())
+            ->setCategory(Publication::CATEGORY_PUBLICATION_ID)
+            ->setAuthor($this->getUser());
+
+        $form = $this->createForm(PublicationType::class, $publication);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->persist($publication);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('user_publications');
+        }
+
+        return $this->render('publications/add.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/publications/{id}", name="publications_show")
+     *
+     * @return Response
+     */
+    public function show(Publication $publication)
+    {
+        return $this->render('publications/show.html.twig', ['publication' => $publication]);
     }
 }
