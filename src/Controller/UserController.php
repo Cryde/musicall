a0bbe9fe4df7eb0f25f6cd4f97c63dc584052b9f
Jérémy <t.jeremy.0@gmail.com>
2019-03-05 08:04:15 +0100

    feat: enable editing + editor on form + customize some list views

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Publication;
 use App\Repository\PublicationRepository;
+use App\Repository\PublicationSubCategoryRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,15 +14,23 @@ class UserController extends AbstractController
 {
     /**
      * @Route("/user/publications", name="user_publications")
+     * @IsGranted("IS_AUTHENTICATED_REMEMBERED")
      *
-     * @param PublicationRepository $publicationRepository
+     * @param PublicationRepository            $publicationRepository
+     * @param PublicationSubCategoryRepository $publicationSubCategoryRepository
      *
      * @return Response
      */
-    public function publications(PublicationRepository $publicationRepository)
-    {
+    public function publications(
+        PublicationRepository $publicationRepository,
+        PublicationSubCategoryRepository $publicationSubCategoryRepository
+    ) {
         $drafts = $publicationRepository->findBy(['author' => $this->getUser(), 'status' => Publication::STATUS_DRAFT]);
+        $subCategories = $publicationSubCategoryRepository->findAll();
 
-        return $this->render('user/publications.html.twig', ['drafts' => $drafts]);
+        return $this->render('user/publications.html.twig', [
+            'drafts'         => $drafts,
+            'sub_categories' => $subCategories,
+        ]);
     }
 }
--- src/Form/PublicationType.php
+++ src/Form/PublicationType.php
@@ -6,6 +6,8 @@ use App\Entity\Publication;
 use App\Entity\PublicationSubCategory;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,9 +16,9 @@ class PublicationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, ['attr' => ['placeholder' => 'Titre'], 'required' => false])
             ->add('shortDescription')
-            ->add('content')
+            ->add('content', TextareaType::class, ['attr' => ['class' => 'js-st-instance'], 'required' => false])
             ->add('subCategory', EntityType::class, [
                 'class'        => PublicationSubCategory::class,
                 'choice_label' => 'title',
