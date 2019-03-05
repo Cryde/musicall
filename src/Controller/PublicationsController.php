68da1e88eaf2f35f80a601905945cbca0e8cb091
Jérémy <t.jeremy.0@gmail.com>
2019-03-05 08:04:15 +0100

    feat: enable editing + editor on form + customize some list views

--- src/Controller/PublicationsController.php
+++ src/Controller/PublicationsController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Publication;
+use App\Entity\PublicationSubCategory;
 use App\Form\PublicationType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,18 +26,38 @@ class PublicationsController extends AbstractController
 
 
     /**
-     * @Route("/publications/add", name="publications_add")
+     * @Route("/publications/{id}/add", name="publications_add")
+     * @IsGranted("IS_AUTHENTICATED_REMEMBERED")
      *
-     * @param Request $request
+     * @param PublicationSubCategory $subCategory
      *
-     * @return Response
+     * @return RedirectResponse
      */
-    public function add(Request $request)
+    public function add(PublicationSubCategory $subCategory)
     {
         $publication = (new Publication())
+            ->setTitle('Votre titre ici')
             ->setCategory(Publication::CATEGORY_PUBLICATION_ID)
-            ->setAuthor($this->getUser());
+            ->setAuthor($this->getUser())
+            ->setSubCategory($subCategory);
 
+        $this->getDoctrine()->getManager()->persist($publication);
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->redirectToRoute('publications_edit', ['id' => $publication->getId()]);
+    }
+
+    /**
+     * @Route("/publications/{id}/edit", name="publications_edit")
+     * @IsGranted("IS_AUTHENTICATED_REMEMBERED")
+     *
+     * @param Publication $publication
+     * @param Request     $request
+     *
+     * @return RedirectResponse|Response
+     */
+    public function edit(Publication $publication, Request $request)
+    {
         $form = $this->createForm(PublicationType::class, $publication);
 
         $form->handleRequest($request);
@@ -51,6 +74,8 @@ class PublicationsController extends AbstractController
 
     /**
      * @Route("/publications/{id}", name="publications_show")
+     *
+     * @param Publication $publication
      *
      * @return Response
      */
