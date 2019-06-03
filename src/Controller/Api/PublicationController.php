cfb5f21cbf01910e52b6572235f57d5745f18880
Jérémy <t.jeremy.0@gmail.com>
2019-06-03 07:37:33 +0200

    wip on image publication

---
+++ src/Controller/Api/PublicationController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PublicationController extends AbstractController
+{
+    /**
+     * @Route("/api/publications/images/add", name="api_publication_images_add")
+     */
+    public function addImageByPublication()
+    {
+
+    }
+}
