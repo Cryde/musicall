1833e2122d7f85c667e3220cf01b026e3c6fd36b
Jérémy <t.jeremy.0@gmail.com>
2019-06-18 08:44:13 +0200

    feat: restore dropdown back

--- src/Controller/PublicationsController.php
+++ src/Controller/PublicationsController.php
@@ -8,7 +8,6 @@ use App\Form\PublicationType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -27,7 +27,7 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="app_logout", methods={"GET"})
+     * @Route("/logout", name="app_logout", methods={"GET"}, options={"expose": true})
      * @throws \Exception
      */
     public function logout()
