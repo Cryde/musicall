a460dc1a84fc862a60bfbf591417e78d66755b6b
Jérémy <t.jeremy.0@gmail.com>
2019-09-30 07:27:16 +0200

    feat: add spinner on publication page

--- assets/js/components/publication/list/List.vue
+++ assets/js/components/publication/list/List.vue
@@ -1,12 +1,18 @@
 <template>
-    <div id="publication-list">
+    <div v-if="isLoading" class="text-center">
+        <b-spinner variant="primary"></b-spinner>
+    </div>
+    <div v-else id="publication-list">
         <b-card-group columns>
             <div v-for="publication in publications" :key="publication.id">
-                <b-card v-if="publication.category !== 'news'" tag="b-link" :to="{ name: 'publication_show', params: { slug: publication.slug }}" :title="publication.title" :img-src="publication.cover_image" img-alt="Image" img-top>
+                <b-card v-if="publication.category !== 'news'" tag="b-link"
+                        :to="{ name: 'publication_show', params: { slug: publication.slug }}" :title="publication.title"
+                        :img-src="publication.cover_image" img-alt="Image" img-top>
                     <b-card-text v-if="publication.type === 'text'">
                         {{ publication.description }}
                     </b-card-text>
-                    <div v-if="publication.type === 'video'" class="text-center video-tag"><i class="fab fa-youtube"></i></div>
+                    <div v-if="publication.type === 'video'" class="text-center video-tag"><i
+                            class="fab fa-youtube"></i></div>
                 </b-card>
                 <b-card v-else>
                     <b-card-title>{{ publication.title }}</b-card-title>
@@ -20,19 +26,20 @@
 </template>
 
 <script>
-    import {mapGetters} from 'vuex';
+  import {mapGetters} from 'vuex';
 
-    export default {
-      metaInfo: {
-        title: 'Publications',
-      },
-      computed: {
-        ...mapGetters('publications', [
-            'publications'
-        ])
-      },
-      async created() {
-        await this.$store.dispatch('publications/getPublications');
-      }
+  export default {
+    metaInfo: {
+      title: 'Publications',
+    },
+    computed: {
+      ...mapGetters('publications', [
+        'publications',
+        'isLoading'
+      ])
+    },
+    async created() {
+      await this.$store.dispatch('publications/getPublications');
     }
+  }
 </script>
