a65c08a36d3c5b9d7a605f63cc32112ed3eb98f1
Jérémy <t.jeremy.0@gmail.com>
2019-10-21 16:59:15 +0200

    refactor: rewrite login store module, commits are now smaller

--- assets/js/store/modules/security.js
+++ assets/js/store/modules/security.js
@@ -1,6 +1,14 @@
 import apiSecurity from '../../api/security';
 import jwt from 'jwt-decode';
 
+const UPDATE_LOCALE_STORAGE = 'UPDATE_LOCALE_STORAGE';
+const UPDATE_IS_LOADING = 'UPDATE_IS_LOADING';
+const UPDATE_ERRORS = 'UPDATE_ERRORS';
+const AUTHENTICATING = 'AUTHENTICATING';
+const UPDATE_AUTH_STATE = 'UPDATE_AUTH_STATE';
+const UPDATE_AUTH_REQUESTED = 'UPDATE_AUTH_REQUESTED';
+const AUTHENTICATING_ERROR = 'AUTHENTICATING_ERROR';
+
 const state = {
   isLoading: false,
   error: null,
@@ -30,71 +38,56 @@ const getters = {
 };
 
 const mutations = {
-  ['AUTHENTICATING'](state) {
-    state.isLoading = true;
-    state.error = null;
+  [AUTHENTICATING](state) {
     state.isAuthenticated = false;
     state.user = {};
   },
-  ['UPDATE_IS_LOADING'](state, isLoading) {
-    state.isLoading = isLoading;
-  },
-  ['AUTHENTICATING_SUCCESS'](state, payload) {
-    state.isLoading = false;
-    state.error = null;
+  [UPDATE_AUTH_STATE](state, payload) {
     state.isAuthenticated = true;
 
     state.token = payload.token;
     state.refreshToken = payload.refresh_token;
-
-    localStorage.setItem('token', payload.token);
-    localStorage.setItem('refresh_token', payload.refresh_token);
-  },
-  ['UPDATE_AUTH_STATE'](state, payload) {
-    state.token = payload.token;
-    state.refreshToken = payload.refresh_token;
-
-    state.isAuthenticated = true;
 
     const jwtDecoded = jwt(state.token);
     state.user = {
       username: jwtDecoded.username,
       roles: jwtDecoded.roles,
     };
-
+  },
+  [UPDATE_LOCALE_STORAGE](state, payload) {
     localStorage.setItem('token', payload.token);
     localStorage.setItem('refresh_token', payload.refresh_token);
   },
-  ['AUTHENTICATING_ERROR'](state, error) {
-    state.isLoading = false;
-    state.error = error;
+  [UPDATE_IS_LOADING](state, isLoading) {
+    state.isLoading = isLoading;
+  },
+  [UPDATE_ERRORS](state, errors) {
+    state.error = errors;
+  },
+  [AUTHENTICATING_ERROR](state) {
     state.isAuthenticated = false;
     state.user = {};
   },
-  ['PROVIDING_DATA_REFRESH'](state, payload) {
-    state.isLoading = false;
-    state.error = null;
-    state.isAuthenticated = payload.isAuthenticated;
-    state.user = payload.user
-  },
-  ['UPDATE_AUTH_REQUESTED'](state, authRequested) {
+  [UPDATE_AUTH_REQUESTED](state, authRequested) {
     state.authRequested = authRequested;
   }
 };
 
 const actions = {
   async login({commit}, payload) {
-    commit('AUTHENTICATING');
+    commit(UPDATE_IS_LOADING, true);
+    commit(UPDATE_ERRORS, null);
+    commit(AUTHENTICATING);
     try {
       const resp = await apiSecurity.login(payload.username, payload.password);
-      commit('AUTHENTICATING_SUCCESS', {token: resp.token, refresh_token: resp.refresh_token});
-      console.log(jwt(resp.token));
+      commit(UPDATE_AUTH_STATE, {token: resp.token, refresh_token: resp.refresh_token});
+      commit(UPDATE_LOCALE_STORAGE, {token: resp.token, refresh_token: resp.refresh_token});
+      commit(UPDATE_IS_LOADING, false);
     } catch (err) {
-      commit('AUTHENTICATING_ERROR', err);
+      commit(AUTHENTICATING_ERROR);
+      commit(UPDATE_ERRORS, err);
+      commit(UPDATE_IS_LOADING, false);
     }
-  },
-  refresh({commit}) {
-    commit('PROVIDING_DATA_REFRESH', payload);
   },
   async getAuthToken({commit, state}, displayLoading) {
 
@@ -102,23 +95,25 @@ const actions = {
       return null;
     }
 
-    displayLoading && commit('UPDATE_IS_LOADING', true);
+    displayLoading && commit(UPDATE_IS_LOADING, true);
     // if the current store token expires soon
     if (isTokenExpired(state.token)) {
       // if not already requesting a token
       if (!state.authRequested) {
-        commit('UPDATE_AUTH_REQUESTED', true);
+        commit(UPDATE_AUTH_REQUESTED, true);
         const resp = await apiSecurity.refreshToken(state.refreshToken);
-        commit('UPDATE_AUTH_REQUESTED', false);
-        displayLoading && commit('UPDATE_IS_LOADING', false);
-        commit('UPDATE_AUTH_STATE', {token: resp.token, refresh_token: resp.refresh_token});
+        commit(UPDATE_AUTH_REQUESTED, false);
+        displayLoading && commit(UPDATE_IS_LOADING, false);
+        commit(UPDATE_AUTH_STATE, {token: resp.token, refresh_token: resp.refresh_token});
+        commit(UPDATE_LOCALE_STORAGE, {token: resp.token, refresh_token: resp.refresh_token});
 
         return resp.token;
       }
     }
 
-    commit('UPDATE_AUTH_STATE', {token: state.token, refresh_token: state.refreshToken});
-    displayLoading && commit('UPDATE_IS_LOADING', false);
+    commit(UPDATE_AUTH_STATE, {token: state.token, refresh_token: state.refreshToken});
+    commit(UPDATE_LOCALE_STORAGE, {token: state.token, refresh_token: state.refreshToken});
+    displayLoading && commit(UPDATE_IS_LOADING, false);
 
     return state.token;
   }
