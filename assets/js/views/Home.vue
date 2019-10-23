fea525c45762c9159bec7d734dd6a347c7c54909
Jérémy <t.jeremy.0@gmail.com>
2019-10-23 09:59:40 +0200

    feat: move the todo to the todo page and make home display the publications

--- assets/js/views/Home.vue
+++ assets/js/views/Home.vue
@@ -2,60 +2,15 @@
     <div>
         <h1>Home</h1>
 
-        <div class="content-box">
-            <p>
-                MusicAll est pour l'instant en phase de re-développement.<br/>
-                C'est à dire que je reprends tout à partir de zero et reconstruit les briques unes à unes.<br/>
-                Le <strong>design</strong> est pour l'instant relativement basique, je prévois de l'améliorer si pas le changer.<br><br/>
+        <b-alert show><i class="fas fa-exclamation-circle"></i> MusicAll est cours de <strong>re-développement</strong>. <router-link :to="{ name: 'todo' }">Pour voir l'avancée c'est ici</router-link></b-alert>
 
-                <strong>Toutes les données que vous rentrerez ici sont suceptible de disparaitre à tout jamais plus tard.</strong>
-            </p>
-            <p>
-                Pour l'instant il n'y a pas énormement de fonctionnalités implémentées.<br/><br/>
-                <strong>Ce qui est fait :</strong>
-            </p>
-            <ul>
-                <li>Possibilité de s'inscrire</li>
-                <li>Possibilité de se connecter</li>
-                <li>Création d'une publication basique</li>
-                <li>Vue d'une publication basique</li>
-                <li>
-                    Publication
-                    <ul>
-                        <li>Mise en place du nouvel éditeur de façon très basique</li>
-                        <li>Edit d'une publication</li>
-                        <li>Suppression d'une publication</li>
-                        <li>Ajouter une ou plusieurs image dans la publication</li>
-                        <li>Vue plus avancée de la publication</li>
-                        <li>Ajouter des messages de confirmation/erreurs lors des diverse actions</li>
-                        <li>Ajouter une photo de "couverture" à la publication</li>
-                    </ul>
-                </li>
-            </ul>
-
-            <p><strong>Ce que je prévois de faire bientôt :</strong></p>
-            <ul>
-                <li>
-                    Publications
-                    <ul>
-                        <li>Gérer le cycle de vie d'une publication (draft - pending - online)</li>
-                        <li>Décliner les publication pour les cours</li>
-                        <li>Commenter une publication</li>
-                    </ul>
-                </li>
-                <li>
-                    User
-                    <ul>
-                        <li>Gérer les accès connecté/non connecté</li>
-                        <li>Ajouter une photo de profil</li>
-                        <li>Se connecter / s'inscrire avec Facebook si on le souhaite</li>
-                        <li>Administration des users (gestion roles - gestion publications)</li>
-                    </ul>
-                </li>
-                <li>Musiciens / Annuaire / Recherche</li>
-                <li>Système de messagerie</li>
-                <li>Forum</li>
-            </ul>
-        </div>
+        <PublicationList/>
     </div>
 </template>
+<script>
+  import PublicationList from "../components/publication/list/PublicationList";
+
+  export default {
+    components: {PublicationList}
+  }
+</script>
