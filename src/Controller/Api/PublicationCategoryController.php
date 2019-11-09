01d4ce8d138bbd9bbb4311df2b0e400d2c185222
Jérémy <t.jeremy.0@gmail.com>
2019-11-09 17:26:36 +0100

    refactor: use the serializer instead of custom made one

--- src/Controller/Api/PublicationCategoryController.php
+++ src/Controller/Api/PublicationCategoryController.php
@@ -3,10 +3,9 @@
 namespace App\Controller\Api;
 
 use App\Repository\PublicationSubCategoryRepository;
-use App\Serializer\PublicationCategoryArraySerializer;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PublicationCategoryController extends AbstractController
@@ -15,20 +14,13 @@ class PublicationCategoryController extends AbstractController
      * @Route("/api/publications/categories", name="api_publication_category_list", options={"expose": true})
      *
      * @param PublicationSubCategoryRepository $publicationSubCategoryRepository
-     * @param PublicationCategoryArraySerializer $publicationCategoryArraySerializer
      *
      * @return JsonResponse
      */
-    public function list(
-        PublicationSubCategoryRepository $publicationSubCategoryRepository,
-        PublicationCategoryArraySerializer $publicationCategoryArraySerializer
-    ) {
+    public function list(PublicationSubCategoryRepository $publicationSubCategoryRepository)
+    {
         $categories = $publicationSubCategoryRepository->findAll();
 
-        return $this->json([
-            'data' => [
-                'categories' => $publicationCategoryArraySerializer->listToArray($categories),
-            ],
-        ]);
+        return $this->json($categories, Response::HTTP_OK, [], ['ignored_attributes' => ['publications', 'id']]);
     }
 }
