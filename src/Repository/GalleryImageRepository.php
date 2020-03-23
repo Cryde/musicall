1f71fb692b4f931a7dadb1cfcb2c877925e50afa
Jérémy <t.jeremy.0@gmail.com>
2020-03-23 07:40:48 +0100

    fix: use correct namespace & remove useless comment

--- src/Repository/GalleryImageRepository.php
+++ src/Repository/GalleryImageRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repository;
 
-use App\Entity\GalleryImage;
+use App\Entity\Image\GalleryImage;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
 
@@ -18,33 +18,4 @@ class GalleryImageRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, GalleryImage::class);
     }
-
-    // /**
-    //  * @return GalleryImage[] Returns an array of GalleryImage objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('g')
-            ->andWhere('g.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('g.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?GalleryImage
-    {
-        return $this->createQueryBuilder('g')
-            ->andWhere('g.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
--- src/Repository/GalleryRepository.php
+++ src/Repository/GalleryRepository.php
@@ -18,33 +18,4 @@ class GalleryRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Gallery::class);
     }
-
-    // /**
-    //  * @return Gallery[] Returns an array of Gallery objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('g')
-            ->andWhere('g.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('g.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Gallery
-    {
-        return $this->createQueryBuilder('g')
-            ->andWhere('g.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
