15efd117c1b5820d89c3a4a56c01c8d56d9fd021
Jérémy <t.jeremy.0@gmail.com>
2020-03-28 11:02:13 +0100

    feat: implement lightbox for galleries

--- assets/js/views/gallery/show/Gallery.vue
+++ assets/js/views/gallery/show/Gallery.vue
@@ -14,9 +14,27 @@
                 <b-spinner variant="primary" label="Spinning"></b-spinner>
             </div>
             <div v-else>
-                <div class="mt-lg-4 mt-3" ref="image-container" v-show="showImages">
-                    <img v-for="image in images" v-lazy="{src: image.sizes.medium, loading: image.sizes.small}"/>
+                <div class="mt-lg-4 mt-3 image-container" ref="image-container" v-show="showImages">
+                    <img
+                            v-for="(image, index) in images"
+                            v-lazy="{src: image.sizes.medium, loading: image.sizes.small}"
+                            :ref="`image-${index}`"
+                            :data-full-image="image.sizes.full"
+                            @click="openLightBox(index)"
+                    />
                 </div>
+            </div>
+
+            <div class="lightbox-container" :class="{'displayed': showLightBox}">
+                <img :src="currentImageLightBox" v-show="!imageLoading"/>
+
+                <span v-show="imageLoading" class="l-loading">
+                    <b-spinner variant="light"/>
+                </span>
+
+                <span @click="nextImage" class="l-next"><i class="fas fa-chevron-right"></i></span>
+                <span @click="prevImage" class="l-prev"><i class="fas fa-chevron-left"></i></span>
+                <span @click="closeImage" class="l-close"><i class="fas fa-times"></i></span>
             </div>
         </div>
     </div>
@@ -31,7 +49,11 @@
     data() {
       return {
         macy: null,
-        showImages: false
+        showImages: false,
+        showLightBox: false,
+        currentImageLightBox: null,
+        currentImageIndex: null,
+        imageLoading: false,
       }
     },
     computed: {
@@ -43,6 +65,7 @@
       ])
     },
     async mounted() {
+      // https://www.cssscript.com/css-only-minimal-responsive-image-gallery-lightbox-cssbox/
       const slug = this.$route.params.slug;
 
       await this.$store.dispatch('gallery/loadGallery', slug);
@@ -65,6 +88,60 @@
         this.macy.recalculate(true);
         this.showImages = true;
       });
+
+      this.enableKeyboardNav();
+    },
+    methods: {
+      openLightBox(index) {
+        this.showLightBox = true;
+        this.currentImageIndex = index;
+        this.openImage();
+      },
+      nextImage() {
+        const nextIndex = this.currentImageIndex + 1;
+        this.currentImageIndex = nextIndex > this.images.length - 1 ? 0 : nextIndex;
+        this.openImage();
+      },
+      prevImage() {
+        const nextIndex = this.currentImageIndex - 1;
+        this.currentImageIndex = nextIndex < 0 ? this.images.length - 1 : nextIndex;
+        this.openImage();
+      },
+      openImage() {
+        this.imageLoading = true;
+        const image = this.$refs[`image-${this.currentImageIndex}`][0];
+        const imageSrc = image.dataset.fullImage;
+        const imageObject = new Image();
+        imageObject.onload = () => {
+          this.currentImageLightBox = imageSrc;
+          this.$nextTick(() => {
+            this.imageLoading = false;
+          });
+        };
+
+        imageObject.src = imageSrc;
+      },
+      closeImage() {
+        this.showLightBox = false;
+        this.currentImageIndex = null;
+        this.currentImageLightBox = null;
+      },
+      enableKeyboardNav() {
+        window.addEventListener('keydown', (e) => {
+          console.log(e.code);
+          if (e.code === 'ArrowRight') {
+            this.showLightBox && this.nextImage();
+          }
+
+          if (e.code === 'ArrowLeft') {
+            this.showLightBox && this.prevImage();
+          }
+
+          if (e.code === 'Escape') {
+            this.showLightBox && this.closeImage();
+          }
+        });
+      }
     },
     filters: {
       dateFormat(date) {
@@ -78,9 +155,102 @@
     .author {
         font-size: 0.8em;
     }
+
+
+    .lightbox-container {
+        position: fixed;
+        z-index: 99999;
+        height: 100%;
+        width: 100%;
+        background-color: rgba(0, 0, 0, 0.8);
+        top: 0;
+        left: 0;
+        opacity: 0;
+        pointer-events: none;
+        transition: opacity 0.5s linear;
+    }
+
+    .lightbox-container.displayed {
+        visibility: visible;
+        opacity: 1;
+        pointer-events: initial;
+    }
+
+    .lightbox-container img {
+        position: fixed;
+        background-color: white;
+        margin: 0;
+        padding: 0;
+        max-height: 90%;
+        max-width: 90%;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+        box-shadow: 0 0 20px black;
+    }
+
+    .l-close,
+    .l-next,
+    .l-prev {
+        position: fixed;
+        text-decoration: none;
+        visibility: hidden;
+        color: white;
+        font-size: 36px;
+        cursor: pointer;
+        transition: color 0.2s;
+    }
+
+    .l-loading {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+    }
+
+    .l-close:hover,
+    .l-next:hover,
+    .l-prev:hover {
+        color: gray;
+    }
+
+    .l-close {
+        top: 1%;
+        right: 1%;
+        font-size: 32px;
+    }
+
+    .l-next,
+    .l-prev {
+        top: 50%;
+        transform: translate(0%, -50%);
+    }
+
+    .l-prev {
+        left: 5%;
+    }
+
+    .l-next {
+        right: 5%;
+    }
+
+    .lightbox-container.displayed .l-close,
+    .lightbox-container.displayed .l-next,
+    .lightbox-container.displayed .l-prev {
+        visibility: visible;
+    }
 </style>
 
 <style>
+    .image-container img {
+        cursor: pointer;
+        transition: opacity .25s ease-in-out;
+    }
+
+    .image-container img:hover {
+        opacity: .6;
+    }
+
     img[lazy=loading] {
         -webkit-filter: blur(2px);
         filter: blur(2px);
