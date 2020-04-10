f2b5d646708205f8b15654a33b7511ffa7560dfd
Jérémy <t.jeremy.0@gmail.com>
2020-04-10 09:09:42 +0200

    refactor: split route into smaller files

---
+++ assets/js/router/publication.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    name: "publication",
+    path: "/publications",
+    component: () => import("../components/publication/list/PublicationList"),
+    meta: {isAuthRequired: false}
+  },
+  {
+    name: "publications_by_category",
+    path: "/publications/category/:slug",
+    component: () => import("../components/publication/list/PublicationList"),
+    meta: {isAuthRequired: false}
+  },
+  {
+    name: "publication_show",
+    path: "/publications/:slug",
+    component: () => import("../components/publication/show/Publication"),
+    meta: {isAuthRequired: false}
+  },
+];
