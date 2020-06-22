cd25712cecdc60ec6d85f8bd5726cc79063bbb9d
Jérémy <t.jeremy.0@gmail.com>
2020-06-22 08:22:56 +0200

    feat: add hide/show menu in small screen

--- assets/js/constants/events.js
+++ assets/js/constants/events.js
@@ -1,4 +1,5 @@
 
+export const EVENT_TOGGLE_MENU = 'app-toggle-menu';
 export const EVENT_ADMIN_ADD_STYLE = 'admin-add-style';
 export const EVENT_ADMIN_ADD_INSTRUMENT = 'admin-add-instrument';
 export const EVENT_ADMIN_ADD_ARTIST = 'admin-add-artist';
