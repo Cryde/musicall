62436586d3dad2c8a2a6e741ea205fe4bd93c3a6
Jérémy <t.jeremy.0@gmail.com>
2020-06-27 22:41:58 +0200

    feat: add a help for admin about the pending gallery & publication in pending mode

--- src/Controller/Api/User/NotificationController.php
+++ src/Controller/Api/User/NotificationController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller\Api\User;
 
+use App\Entity\Gallery;
+use App\Entity\Publication;
+use App\Repository\GalleryRepository;
 use App\Repository\Message\MessageThreadMetaRepository;
+use App\Repository\PublicationRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -16,12 +20,32 @@ class NotificationController extends AbstractController
      * @IsGranted("IS_AUTHENTICATED_REMEMBERED")
      *
      * @param MessageThreadMetaRepository $messageThreadMetaRepository
+     * @param GalleryRepository           $galleryRepository
+     * @param PublicationRepository       $publicationRepository
      *
      * @return JsonResponse
      */
-    public function notifications(MessageThreadMetaRepository $messageThreadMetaRepository)
-    {
+    public function notifications(
+        MessageThreadMetaRepository $messageThreadMetaRepository,
+        GalleryRepository $galleryRepository,
+        PublicationRepository $publicationRepository
+    ) {
         $unreadMessagesCount = $messageThreadMetaRepository->count(['user' => $this->getUser(), 'isRead' => 0]);
+
+        if ($this->isGranted('ROLE_ADMIN')) {
+            $pendingGalleriesCount = $galleryRepository->count(['status' => Gallery::STATUS_PENDING]);
+            $pendingPublicationCount = $publicationRepository->count(['status' => Publication::STATUS_PENDING]);
+
+            return $this->json([
+                'data' => [
+                    'messages' => $unreadMessagesCount,
+                    'admin'    => [
+                        'pending_gallery'     => $pendingGalleriesCount,
+                        'pending_publication' => $pendingPublicationCount,
+                    ],
+                ],
+            ]);
+        }
 
         return $this->json(['data' => [
             'messages' => $unreadMessagesCount
