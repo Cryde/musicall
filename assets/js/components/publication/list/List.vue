d9d2b180f72aa7884e16620830b91693d123134d
Jérémy <t.jeremy.0@gmail.com>
2020-07-04 13:12:15 +0200

    feat: use new mansory lib

--- assets/js/components/publication/list/List.vue
+++ assets/js/components/publication/list/List.vue
@@ -5,30 +5,32 @@
     <div v-else id="publication-list">
         <h1 v-if="currentCategory && !this.isHome">{{ currentCategory.title }}</h1>
         <h1 v-else-if="!this.isHome">Publications</h1>
-        <div class="mt-4" ref="publication-container" v-show="showContainer">
-            <b-card
-                    v-for="publication in publications" :key="publication.id"
-                    v-if="publication.category !== 'news'" tag="b-link"
-                    :to="{ name: 'publication_show', params: { slug: publication.slug }}" :title="publication.title"
-                    :img-src="publication.cover_image" img-alt="Image" img-top>
-                <b-card-text v-if="publication.type === 'text'">
-                    {{ publication.description }}
-                </b-card-text>
-                <div class="publication-date mt-1">{{publication.publication_datetime | relativeDate }}</div>
-                <publication-type :type="publication.type" class="mt-1 pull-right"/>
-            </b-card>
-            <b-card v-else
-                    tag="b-link"
-                    :to="{ name: 'publication_show', params: { slug: publication.slug }}"
-            >
-                <b-card-title>{{ publication.title }}</b-card-title>
-                <b-card-text>
-                    {{ publication.description }}
-                </b-card-text>
-                <div class="publication-date mt-1">{{publication.publication_datetime | relativeDate }}</div>
-            </b-card>
-        </div>
-        <div class="overflow-auto" v-if="numberOfPages > 1" v-show="showContainer">
+        <vue-masonry-wall :items="publications" :options="{padding: 5}" class="mt-4" @append="append">
+            <template v-slot:default="{item: publication}">
+                <b-card
+                        :key="publication.id"
+                        v-if="publication.category !== 'news'" tag="b-link"
+                        :to="{ name: 'publication_show', params: { slug: publication.slug }}" :title="publication.title"
+                        :img-src="publication.cover_image" img-alt="Image" img-top>
+                    <b-card-text v-if="publication.type === 'text'">
+                        {{ publication.description }}
+                    </b-card-text>
+                    <div class="publication-date mt-1">{{publication.publication_datetime | relativeDate }}</div>
+                    <publication-type :type="publication.type" class="mt-1 pull-right"/>
+                </b-card>
+                <b-card v-else
+                        tag="b-link"
+                        :to="{ name: 'publication_show', params: { slug: publication.slug }}"
+                >
+                    <b-card-title>{{ publication.title }}</b-card-title>
+                    <b-card-text>
+                        {{ publication.description }}
+                    </b-card-text>
+                    <div class="publication-date mt-1">{{publication.publication_datetime | relativeDate }}</div>
+                </b-card>
+            </template>
+        </vue-masonry-wall>
+        <div class="overflow-auto mt-3" v-if="numberOfPages > 1">
             <b-pagination-nav :link-gen="linkGen" :number-of-pages="numberOfPages" use-router align="right"
                               size="sm"></b-pagination-nav>
         </div>
@@ -38,16 +40,15 @@
 <script>
   import {mapGetters} from 'vuex';
   import PublicationType from "../PublicationType";
-  import Macy from "macy";
+  import VueMasonryWall from "vue-masonry-wall";
 
   export default {
-    components: {PublicationType},
+    components: {PublicationType, VueMasonryWall},
     data() {
       return {
         macy: null,
         isHome: false,
         currentCategory: null,
-        showContainer: false,
       }
     },
     metaInfo() {
@@ -65,8 +66,6 @@
     },
     watch: {
       $route() {
-        this.macy && this.macy.remove();
-        this.macy = null;
         this.fetchData();
       },
     },
@@ -80,6 +79,12 @@
       });
     },
     methods: {
+      async append() {
+        return this.publications;
+        console.log('appned !!');
+        //await this.fetchData();
+       // return this.publications;
+      },
       pageTitle() {
         if (this.isHome) {
           return 'MusicAll, le site de référence au service de la musique';
@@ -100,35 +105,12 @@
         } else {
           await this.$store.dispatch('publications/getPublications', {offset});
         }
-        await this.initMacy();
-      },
-      async initMacy() {
-        this.showContainer = false;
-        this.macy = Macy({
-          container: this.$refs['publication-container'],
-          trueOrder: true,
-          waitForImages: false,
-          margin: 14,
-          columns: 3,
-          breakAt: {
-            1200: 4,
-            940: 3,
-            560: 2,
-            400: 1
-          }
-        });
-        await this.macy.recalculateOnImageLoad(true);
-        this.showContainer = true;
-        this.macy.runOnImageLoad(() => {
-        });// we have to let this
       },
       linkGen(pageNum) {
         return pageNum === 1 ? '?' : `?page=${pageNum}`
       }
     },
     beforeDestroy() {
-      this.macy = null;
-      this.showContainer = false;
       this.currentCategory = null;
     }
   }
