bf1650b64cbb19b2a240d1434ce216b4e869d993
Jérémy <t.jeremy.0@gmail.com>
2020-09-15 07:46:18 +0200

    chore: update php libs + fix some bad namespace

--- src/Repository/PublicationRepository.php
+++ src/Repository/PublicationRepository.php
@@ -5,7 +5,7 @@ namespace App\Repository;
 use App\Entity\Publication;
 use App\Entity\PublicationSubCategory;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\ORM\NoResultException;
 
