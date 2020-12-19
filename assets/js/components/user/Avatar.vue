9f9944110259975d86434a957126cddab1390daf
Jérémy <t.jeremy.0@gmail.com>
2020-12-19 11:43:03 +0100

    fix: issue with loaded js/css files & avatar

--- assets/js/components/user/Avatar.vue
+++ assets/js/components/user/Avatar.vue
@@ -1,6 +1,6 @@
 <template>
-  <span v-if="isLoading"><b-spinner/></span>
-  <div v-else-if="!userPicture" class="image circle" :class="cssClass">
+  <span v-if="isLoading"><spinner/></span>
+  <div v-else-if="!userPicture" class="image circled-image has-text-centered" :class="cssClass">
     <span class="initials">{{ user.username[0].toLocaleUpperCase() }}</span>
   </div>
   <p v-else class="image" :class="cssClass">
@@ -9,7 +9,10 @@
 </template>
 
 <script>
+import Spinner from "../global/misc/Spinner";
+
 export default {
+  components: {Spinner},
   props: {
     user: Object,
     isLoading: Boolean,
@@ -46,7 +49,7 @@ export default {
 
 <style>
 
-.circle {
+.circled-image {
   background-color: #ccc;
   border-radius: 50%;
   text-align: center;
