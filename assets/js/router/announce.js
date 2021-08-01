c2a0fb7f42db3bb70cee47ac5cc4574976c9ba18
Jérémy <t.jeremy.0@gmail.com>
2021-08-01 08:59:45 +0200

    refactor: remove old 'create announce' route

--- assets/js/router/announce.js
+++
@@ -1,8 +0,0 @@
-export default [
-  {
-    path: "/annonce/musicien",
-    name: "announce_musician_add",
-    component: () => import("../views/announce/musician/Add"),
-    meta: {isAuthRequired: false}
-  },
-];
