98a9fd6e9e32ea2d0ee45887bfe6d7d1d23ffcb3
Jérémy <t.jeremy.0@gmail.com>
2022-01-14 10:01:06 +0100

    fix: partially fix admin view

--- assets/js/views/admin/gallery/GalleryPending.vue
+++ assets/js/views/admin/gallery/GalleryPending.vue
@@ -1,45 +1,59 @@
 <template>
     <div>
-        <h1>
-            <b-link :to="{name: 'admin_dashboard'}">Admin</b-link>
-            / Gallery en attente de validation
-        </h1>
+      <breadcrumb
+          :root="{to: {name: 'admin_dashboard'}, label: 'Admin'}"
+          :current="{label: 'Gallery en attente de validation'}"
+      />
+      <h1 class="subtitle is-3">Gallery en attente de validation</h1>
 
-        <b-table class="mt-5" striped hover :fields="fields" :items="galleries" :busy="isLoading" show-empty>
-            <template v-slot:table-busy>
-                <div class="has-text-centered text-danger my-2">
-                    <b-spinner class="align-middle"></b-spinner>
-                </div>
-            </template>
 
-            <template v-slot:empty="scope">
-                <h4>Il n'y a pas de galeries en attente !</h4>
-            </template>
+      <b-table :data="galleries" :loading="isLoading" mobile-cards>
 
-            <template v-slot:cell(author)="data">
-                {{ data.item.author.username }}
-            </template>
-            <template v-slot:cell(actions)="data">
-                <b-button-group size="sm">
-                    <b-button variant="outline-primary" v-b-tooltip.hover title="Voir la galerie"
-                              :to="{ name: 'gallery_show', params: { slug: data.item.slug }}"
-                              target="_blank">
-                        <i class="far fa-eye fa-fw"></i>
-                    </b-button>
+        <b-table-column field="title" label="Titre" sortable v-slot="props">
+          {{ props.row.title }}
+        </b-table-column>
+        <b-table-column field="author" label="Auteur" sortable v-slot="props">
+          {{ props.row.author.username }}
+        </b-table-column>
 
-                    <b-button variant="outline-success" v-b-tooltip.hover title="Valider la publication"
-                              @click="confirmApprove(data.item.id)">
-                        <i class="fas fa-check fa-fw"></i>
-                    </b-button>
+        <b-table-column label="Actions" v-slot="props">
+          <b-field size="is-small">
+            <p class="control">
+              <b-tooltip label="Voir la galerie" type="is-black">
+                <b-button size="is-small" type="is-info is-light"
+                          target="_blank" tag="router-link"
+                          icon-left="eye"
+                          :to="{ name: 'gallery_show', params: { slug: props.row.slug }}">
+                </b-button>
+              </b-tooltip>
+            </p>
 
-                    <b-button variant="outline-danger" v-b-tooltip.hover title="Rejeter la publication"
-                              @click="confirmReject(data.item.id)">
-                        <i class="fas fa-times fa-fw"></i>
-                    </b-button>
+            <p class="control">
+              <b-tooltip label="Valider la galerie" type="is-black">
+                <b-button size="is-small" type="is-success is-light"
+                          icon-left="check"
+                          @click="confirmApprove(props.row.id)">
+                </b-button>
+              </b-tooltip>
+            </p>
 
-                </b-button-group>
-            </template>
-        </b-table>
+            <p class="control">
+              <b-tooltip label="Rejeter la galerie" type="is-black">
+                <b-button size="is-small" type="is-danger is-light"
+                          icon-left="times"
+                          @click="confirmReject(props.row.id)">
+                </b-button>
+              </b-tooltip>
+            </p>
+          </b-field>
+        </b-table-column>
+
+        <template #empty>
+          <div class="has-text-centered" v-if="!isLoading">
+            Il n'y a pas de galleries en attente !
+          </div>
+        </template>
+      </b-table>
     </div>
 </template>
 
