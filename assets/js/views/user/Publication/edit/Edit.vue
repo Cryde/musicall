4eb965e5183b2311c92391c611b480d2a4fdd80e
Jérémy <t.jeremy.0@gmail.com>
2022-11-13 08:53:27 +0100

    refactor: move to the new tiptap editor

--- assets/js/views/user/Publication/edit/Edit.vue
+++ assets/js/views/user/Publication/edit/Edit.vue
@@ -4,184 +4,153 @@
   </div>
   <div v-else>
     <b-loading :active="isPublishing"/>
-    <b-button icon-left="cog" class="is-pulled-right" @click="$refs['modal-publication-properties'].open()">Configuration</b-button>
+    <b-button icon-left="cog" class="is-pulled-right" @click="$refs['modal-publication-properties'].open()">
+      Configuration
+    </b-button>
     <h1 class="subtitle is-3">
       <router-link :to="{name:'user_publications'}" class="mr-2"><i class="fas fa-chevron-left"></i></router-link>
       {{ title }}
     </h1>
-    <div class="box content is-shadowless p-lg-3 p-3">
-      <div class="editor">
-        <editor-menu-bubble class="menububble" :editor="editor" @hide="hideLinkMenu"
-                            v-slot="{ commands, isActive, getMarkAttrs, menu }">
-          <div
-              class="menububble"
-              :class="{ 'is-active': menu.isActive }"
-              :style="`left: ${menu.left}px; bottom: ${menu.bottom}px;`"
-          >
+    <div class="box content is-shadowless p-lg-3 p-3" v-if="editor">
+      <div>
+        <div class="menubar buttons">
+          <div class="buttons has-addons mr-2 mb-0">
+            <b-tooltip label="Gras" type="is-black">
+              <b-button size="is-small" type="is-info" icon-left="bold"
+                        @click="editor.chain().focus().toggleBold().run()"
+                        :class="{ 'is-light': !editor.isActive('bold') }"/>
+            </b-tooltip>
 
-            <form class="menububble__form" v-if="linkMenuIsActive"
-                  @submit.prevent="setLinkUrl(commands.link, linkUrl)">
-              <input class="menububble__input" type="text" v-model="linkUrl" placeholder="https://"
-                     ref="linkInput" @keydown.esc="hideLinkMenu"/>
-              <button class="menububble__button" @click="setLinkUrl(commands.link, null)" type="button">
-                <i class="fas fa-times"></i>
-              </button>
-            </form>
+            <b-tooltip label="Italique" type="is-black">
+              <b-button size="is-small" type="is-info" icon-left="italic"
+                        @click="editor.chain().focus().toggleItalic().run()"
+                        :class="{ 'is-light': !editor.isActive('italic') }"/>
+            </b-tooltip>
 
-            <template v-else>
-              <button
-                  class="menububble__button"
-                  @click="showLinkMenu(getMarkAttrs('link'))"
-                  :class="{ 'is-active': isActive.link() }"
-              >
-                <span>{{ isActive.link() ? 'Mettre à jour le lien' : 'Ajouter un lien' }}</span>
-                <i class="fas fa-link"></i>
-              </button>
-            </template>
+            <b-tooltip label="Paragraphe" type="is-black">
+              <b-button size="is-small" type="is-info" icon-left="paragraph"
+                        @click="editor.chain().focus().setParagraph().run()"
+                        :class="{ 'is-light': !editor.isActive('paragraph') }"/>
+            </b-tooltip>
+          </div>
+
+          <div class="buttons has-addons mr-2 mb-0">
+            <b-tooltip label="Titre niveau 2" type="is-black">
+              <b-button size="is-small" type="is-info"
+                        :class="{ 'is-light': !editor.isActive('heading', { level: 2 }) }"
+                        @click="editor.chain().focus().toggleHeading({ level: 2 }).run()">
+                H2
+              </b-button>
+            </b-tooltip>
+
+            <b-tooltip label="Titre niveau 3" type="is-black">
+              <b-button size="is-small" type="is-info"
+                        :class="{ 'is-light': !editor.isActive('heading', { level: 3 }) }"
+                        @click="editor.chain().focus().toggleHeading({ level: 3 }).run()">
+                H3
+              </b-button>
+            </b-tooltip>
+          </div>
+          <div class="buttons has-addons mr-2 mb-0">
+            <b-tooltip label="Aligner à gauche" type="is-black">
+              <b-button size="is-small" type="is-info" icon-left="align-left"
+                        :class="{ 'is-light': !editor.isActive({ textAlign: 'left' }) }"
+                        @click="editor.chain().focus().setTextAlign('left').run()"/>
+            </b-tooltip>
+
+            <b-tooltip label="Aligner au centre" type="is-black">
+              <b-button size="is-small" type="is-info" icon-left="align-center"
+                        :class="{ 'is-light': !editor.isActive({ textAlign: 'center' }) }"
+                        @click="editor.chain().focus().setTextAlign('center').run()"/>
+            </b-tooltip>
+
+            <b-tooltip label="Aligner à droite" type="is-black">
+              <b-button size="is-small" type="is-info" icon-left="align-right"
+                        :class="{ 'is-light': !editor.isActive({ textAlign: 'right' }) }"
+                        @click="editor.chain().focus().setTextAlign('right').run()"/>
+            </b-tooltip>
+          </div>
+
+          <div class="buttons has-addons mr-2 mb-0">
+            <b-tooltip label="Liste à puce" type="is-black">
+              <b-button size="is-small" type="is-info" icon-left="list-ul"
+                        :class="{ 'is-light': !editor.isActive('bulletList') }"
+                        @click="editor.chain().focus().toggleBulletList().run()"/>
+            </b-tooltip>
+
+            <b-tooltip label="Liste numérotée" type="is-black">
+              <b-button size="is-small" type="is-info" icon-left="list-ol"
+                        @click="editor.chain().focus().toggleOrderedList().run()"
+                        :class="{ 'is-light': !editor.isActive('orderedList') }"/>
+            </b-tooltip>
+          </div>
+
+          <div class="buttons has-addons mr-2 mb-0">
+            <b-tooltip label="Citation" type="is-black">
+              <b-button size="is-small" type="is-info" icon-left="quote-right"
+                        @click="editor.chain().focus().toggleBlockquote().run()"
+                        :class="{ 'is-light': !editor.isActive('blockquote') }"/>
+            </b-tooltip>
+
+            <b-tooltip label="Séparation" type="is-black">
+              <b-button size="is-small" type="is-info is-light"
+                        @click="editor.chain().focus().setHorizontalRule().run()">_
+              </b-button>
+            </b-tooltip>
+          </div>
+
+          <div class="buttons has-addons mr-2 mb-0">
+            <b-tooltip label="Insérer une image" type="is-black">
+              <b-button size="is-small" type="is-info is-light" icon-left="image"
+                        @click="$refs.file.click()"/>
+            </b-tooltip>
+            <b-tooltip label="Insérer une vidéo YouTube" type="is-black">
+              <b-button size="is-small" type="is-info is-light" icon-left="youtube" icon-pack="fab"
+                        @click="showVideoModal()"/>
+            </b-tooltip>
+          </div>
+
+          <div class="buttons has-addons mb-0 mr-5 ">
+            <b-tooltip label="Défaire" type="is-black">
+              <b-button size="is-small" type="is-info is-light" icon-left="undo"
+                        @click="editor.chain().focus().undo().run()" :disabled="!editor.can().undo()"/>
+            </b-tooltip>
+
+            <b-tooltip label="Refaire" type="is-black">
+              <b-button size="is-small" type="is-info is-light" icon-left="redo"
+                        @click="editor.chain().focus().redo().run()"
+                        :disabled="!editor.can().redo()"/>
+            </b-tooltip>
+          </div>
+
+
+          <div class="buttons mb-0 ml-5 is-flex-grow-1 is-justify-content-end">
+            <b-button size="is-small"
+                      type="is-success" icon-left="paper-plane"
+                      :loading="isPublishing" :disabled="isPublishing"
+                      @click="publish">
+              Publier
+            </b-button>
+
+            <b-button size="is-small"
+                      type="is-success" icon-left="save"
+                      :loading="isSaving" :disabled="isSaving"
+                      @click="save">
+              Enregistrer
+            </b-button>
+
+            <b-button size="is-small"
+                      type="is-info" icon-left="eye" tag="router-link" target="_blank"
+                      :to="{ name: 'publication_show', params: { slug: slug }}"/>
 
           </div>
-        </editor-menu-bubble>
-
-        <div class="editor-sticky" v-sticky sticky-offset="offset" sticky-side="top" :sticky-z-index="39"
-             v-if="content">
-          <editor-menu-bar :editor="editor" v-slot="{ commands, isActive, getMarkAttrs }">
-
-            <div class="menubar buttons">
-
-              <div class="buttons has-addons mr-2 mb-0">
-                <b-tooltip label="Gras" type="is-black">
-                  <b-button size="is-small" type="is-info" icon-left="bold"
-                            :class="isActive.bold() ? '' : 'is-light'" @click="commands.bold"/>
-                </b-tooltip>
-
-                <b-tooltip label="Italique" type="is-black">
-                  <b-button size="is-small" type="is-info" icon-left="italic"
-                            :class="isActive.italic() ? '' : 'is-light'" @click="commands.italic"/>
-                </b-tooltip>
-
-                <b-tooltip label="Paragraphe" type="is-black">
-                  <b-button size="is-small" type="is-info" icon-left="paragraph"
-                            :class="isActive.paragraph() ? '' : 'is-light'" @click="commands.paragraph"/>
-                </b-tooltip>
-              </div>
-
-              <div class="buttons has-addons mr-2 mb-0">
-                <b-tooltip label="Titre niveau 2" type="is-black">
-                  <b-button size="is-small" type="is-info"
-                            :class="isActive.heading({ level: 2 }) ? '' : 'is-light'"
-                            @click="commands.heading({ level: 2 })">
-                    H2
-                  </b-button>
-                </b-tooltip>
-
-                <b-tooltip label="Titre niveau 3" type="is-black">
-                  <b-button size="is-small" type="is-info"
-                            :class="isActive.heading({ level: 3 }) ? '' : 'is-light'"
-                            @click="commands.heading({ level: 3 })">
-                    H3
-                  </b-button>
-                </b-tooltip>
-              </div>
-
-              <div class="buttons has-addons mr-2 mb-0">
-                <b-tooltip label="Aligner à gauche" type="is-black">
-                  <b-button size="is-small" type="is-info" icon-left="align-left"
-                            :class="getMarkAttrs('align').textAlign === 'left' ? '' : 'is-light'"
-                            @click="commands.align({textAlign: 'left'})"/>
-                </b-tooltip>
-
-                <b-tooltip label="Aligner au centre" type="is-black">
-                  <b-button size="is-small" type="is-info" icon-left="align-center"
-                            :class="getMarkAttrs('align').textAlign === 'centered' ? '' : 'is-light'"
-                            @click="commands.align({textAlign: 'centered'})"/>
-                </b-tooltip>
-
-                <b-tooltip label="Aligner à droite" type="is-black">
-                  <b-button size="is-small" type="is-info" icon-left="align-right"
-                            :class="getMarkAttrs('align').textAlign === 'right' ? '' : 'is-light'"
-                            @click="commands.align({textAlign: 'right'})"/>
-                </b-tooltip>
-              </div>
-
-              <div class="buttons has-addons mr-2 mb-0">
-                <b-tooltip label="Liste à puce" type="is-black">
-                  <b-button size="is-small" type="is-info" icon-left="list-ul"
-                            :class="isActive.bullet_list() ? '' : 'is-light'" @click="commands.bullet_list"/>
-                </b-tooltip>
-
-                <b-tooltip label="Liste numérotée" type="is-black">
-                  <b-button size="is-small" type="is-info" icon-left="list-ol"
-                            :class="isActive.ordered_list() ? '' : 'is-light'" @click="commands.ordered_list"/>
-                </b-tooltip>
-              </div>
-
-
-              <div class="buttons has-addons mr-2 mb-0">
-                <b-tooltip label="Citation" type="is-black">
-                  <b-button size="is-small" type="is-info" icon-left="quote-right"
-                            :class="isActive.blockquote() ? '' : 'is-light'" @click="commands.blockquote"/>
-                </b-tooltip>
-
-                <b-tooltip label="Séparation" type="is-black">
-                  <b-button size="is-small" type="is-info is-light"
-                            @click="commands.horizontal_rule">_
-                  </b-button>
-                </b-tooltip>
-              </div>
-
-              <div class="buttons has-addons mr-2 mb-0">
-                <b-tooltip label="Insérer une image" type="is-black">
-                  <b-button size="is-small" type="is-info" icon-left="image"
-                            :class="isActive.image() ? '' : 'is-light'" @click="$refs.file.click()"/>
-                </b-tooltip>
-
-                <b-tooltip label="Insérer une vidéo YouTube" type="is-black">
-                  <b-button size="is-small" type="is-info" icon-left="youtube" icon-pack="fab"
-                            :class="isActive.youtubeiframe() ? '' : 'is-light'"
-                            @click="showVideoModal(commands.youtubeiframe)"/>
-                </b-tooltip>
-              </div>
-
-              <div class="buttons has-addons mb-0 mr-5 ">
-                <b-tooltip label="Défaire" type="is-black">
-                  <b-button size="is-small" type="is-info is-light" icon-left="undo"
-                            @click="commands.undo"/>
-                </b-tooltip>
-
-                <b-tooltip label="Refaire" type="is-black">
-                  <b-button size="is-small" type="is-info is-light" icon-left="redo"
-                            @click="commands.redo"/>
-                </b-tooltip>
-              </div>
-
-              <div class="buttons mb-0 ml-5 is-flex-grow-1 is-justify-content-end">
-                <b-button size="is-small"
-                          type="is-success" icon-left="paper-plane"
-                          :loading="isPublishing" :disabled="isPublishing"
-                          @click="publish">
-                  Publier
-                </b-button>
-
-                <b-button size="is-small"
-                          type="is-success" icon-left="save"
-                          :loading="isSaving" :disabled="isSaving"
-                          @click="save">
-                  Enregistrer
-                </b-button>
-
-                <b-button size="is-small"
-                          type="is-info" icon-left="eye" tag="router-link" target="_blank"
-                          :to="{ name: 'publication_show', params: { slug: slug }}"/>
-
-              </div>
-            </div>
-          </editor-menu-bar>
         </div>
 
-        <editor-content :editor="editor" class="mt-3 publication-container"/>
       </div>
 
+      <editor-content :editor="editor" class="mt-3 publication-container"/>
     </div>
+
     <input type="file" class="is-hidden" ref="file" @change="uploadImage($event)" accept="image/*">
     <edit-modal ref="modal-publication-properties"/>
   </div>
@@ -191,31 +160,17 @@
 import Sticky from 'vue-sticky-directive';
 import axios from 'axios';
 import {mapGetters} from 'vuex';
-import {Editor, EditorContent, EditorMenuBar, EditorMenuBubble} from 'tiptap'
-import {
-  Blockquote,
-  Bold,
-  BulletList,
-  HardBreak,
-  Heading,
-  History,
-  HorizontalRule,
-  Image,
-  Italic,
-  Link,
-  ListItem,
-  OrderedList
-} from 'tiptap-extensions';
+import StarterKit from '@tiptap/starter-kit'
+import TextAlign from '@tiptap/extension-text-align'
+import {Editor, EditorContent} from '@tiptap/vue-2'
 import EditModal from './modal/EditModal';
+import Image from '@tiptap/extension-image'
 import YoutubeIframe from "../../../../tiptap/YoutubeIframe";
-import Align from "../../../../tiptap/Align";
 import {youtubeParser} from '../../../../helper/youtube-parser-url';
 
 export default {
   components: {
     EditorContent,
-    EditorMenuBar,
-    EditorMenuBubble,
     EditModal,
   },
   directives: {Sticky},
@@ -223,30 +178,7 @@ export default {
     return {
       offset: {top: 90},
       contentLoaded: false,
-      editor: new Editor({
-        extensions: [
-          new Blockquote(),
-          new BulletList(),
-          new HardBreak(),
-          new Heading({levels: [2, 3]}),
-          new HorizontalRule(),
-          new ListItem(),
-          new OrderedList(),
-          new Link(),
-          new Bold(),
-          new Italic(),
-          new Link(),
-          new History(),
-          new Image(),
-          new YoutubeIframe(),
-          new Align()
-        ],
-        onUpdate: ({getHTML}) => {
-          // get new content on update
-          this.$store.dispatch('publicationEdit/updateContent', getHTML());
-        },
-        content: '',
-      }),
+      editor: null,
       linkUrl: null,
       linkMenuIsActive: false,
     }
@@ -276,7 +208,23 @@ export default {
         return;
       }
 
-      this.editor.setContent(this.content);
+      this.editor = new Editor({
+        extensions: [
+          StarterKit.configure({
+            heading: [2, 3]
+          }),
+          TextAlign.configure({
+            types: ['paragraph'],
+          }),
+          Image,
+          YoutubeIframe,
+        ],
+        onUpdate: ({editor}) => {
+          // get new content on update
+          this.$store.dispatch('publicationEdit/updateContent', editor.getHTML());
+        },
+        content: this.content,
+      })
     } catch (e) {
       console.error(e);
     }
@@ -346,7 +294,7 @@ export default {
         })
       }
     },
-    async uploadImage() {
+    async uploadImage(event) {
 
       this.$Progress.start()
       const input = event.target;
@@ -368,10 +316,8 @@ export default {
             }
           });
 
-          this.addCommand({
-            command: this.editor.commands.image,
-            data: {src: data.uri,}
-          });
+          this.editor.chain().focus().setImage({src: data.uri}).run();
+
           beginToast.close();
           this.$Progress.finish();
           this.$buefy.toast.open({
@@ -415,10 +361,9 @@ export default {
 
       const youtubeId = youtubeParser(result);
 
-      this.addCommand({
-        command,
-        data: {src: `https://www.youtube.com/embed/${youtubeId}`}
-      })
+      this.editor.chain().focus().addYoutubeVideo({
+        src: `https://www.youtube.com/embed/${youtubeId}`,
+      }).run()
 
       dialog.close();
     },
