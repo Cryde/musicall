ab3be538eb2f430578f4c0b5797b5de53150e0bd
Jérémy <t.jeremy.0@gmail.com>
2023-04-15 09:10:26 +0200

    feat(#518): implement musician announce list for current user

---
+++ src/Extensions/Doctrine/Musician/MusicianAnnounceSelfExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Extensions\Doctrine\Musician;
+
+use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Metadata\Operation;
+use App\Entity\Musician\MusicianAnnounce;
+use App\Entity\User;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Bundle\SecurityBundle\Security;
+
+class MusicianAnnounceSelfExtension implements QueryCollectionExtensionInterface
+{
+    public function __construct(private readonly Security $security)
+    {
+    }
+
+    public function applyToCollection(
+        QueryBuilder                $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string                      $resourceClass,
+        Operation                   $operation = null,
+        array                       $context = []
+    ): void {
+        if (MusicianAnnounce::class !== $resourceClass
+            && $operation->getName() !== 'api_musician_announces_get_self_collection') {
+            return;
+        }
+        /** @var User $user */
+        $user = $this->security->getUser();
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        $queryBuilder->andWhere(sprintf('%s.author = :author', $rootAlias));
+        $queryBuilder->setParameter('author', $user);
+    }
+}
