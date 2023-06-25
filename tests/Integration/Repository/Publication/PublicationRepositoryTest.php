2615760861fe61732e51a1792c5d981ef5d331ae
Jérémy <t.jeremy.0@gmail.com>
2023-06-25 10:12:30 +0200

    test: add test for old redirection & test for publication repository

---
+++ tests/Integration/Repository/Publication/PublicationRepositoryTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests\Integration\Repository\Publication;
+
+use App\Entity\Publication;
+use App\Repository\PublicationRepository;
+use App\Tests\Factory\Publication\PublicationFactory;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class PublicationRepositoryTest extends KernelTestCase
+{
+    use ResetDatabase, Factories;
+
+    public function test_find_one_video()
+    {
+        $repo = static::getContainer()->get(PublicationRepository::class);
+
+        $video = PublicationFactory::createOne(['content' => 'content-id', 'type' => Publication::TYPE_VIDEO]);
+        PublicationFactory::createOne(['content' => 'content-id', 'type' => Publication::TYPE_TEXT]);
+
+        $result = $repo->findOneVideo('content-id');
+        $this->assertSame($video->object(), $result);
+    }
+}
