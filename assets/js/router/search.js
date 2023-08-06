2016050c6aa5e88b0ac363e9de5913abbe74e10d
Jérémy <t.jeremy.0@gmail.com>
2023-08-06 10:47:09 +0200

    refactor: remove old way to search musician

--- assets/js/router/search.js
+++ assets/js/router/search.js
@@ -1,11 +1,5 @@
 export default [
   {
-    path: "/rechercher-un-musicien-legacy/",
-    name: "search_index_legacy",
-    component: () => import("../views/search/Musician/Search"),
-    meta: {isAuthRequired: false}
-  },
-  {
     path: "/rechercher-un-musicien/",
     name: "search_index",
     component: () => import("../views/search/Musician/SearchForm.vue"),
--- src/Controller/Api/SearchController.php
+++ src/Controller/Api/SearchController.php
@@ -2,27 +2,16 @@
 
 namespace App\Controller\Api;
 
-use App\Entity\User;
-use App\Model\Search\Musician;
-use App\Repository\Musician\MusicianAnnounceRepository;
 use App\Repository\PublicationRepository;
 use App\Serializer\PublicationSerializer;
-use App\Serializer\Search\MusicianSearchArraySerializer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class SearchController extends AbstractController
 {
-    public function __construct(
-        private readonly SerializerInterface           $serializer,
-        private readonly MusicianSearchArraySerializer $musicianSearchArraySerializer
-    ) {
-    }
-
     #[Route(path: '/api/search', name: 'api_search', options: ['expose' => true])]
     public function search(
         Request               $request,
@@ -36,20 +25,5 @@ class SearchController extends AbstractController
         $publications = $publicationRepository->getBySearchTerm($term);
 
         return $this->json($publicationSerializer->listToArray($publications));
-    }
-
-    #[Route(path: '/api/search/musician', name: 'api_search_musician', options: ['expose' => true], methods: ['POST'])]
-    public function searchMusician(
-        Request                    $request,
-        MusicianAnnounceRepository $musicianAnnounceRepository,
-    ): JsonResponse {
-        /** @var Musician $musicianModel */
-        $musicianModel = $this->serializer->deserialize($request->getContent(), Musician::class, 'json');
-        /** @var User|null $user */
-        $user = $this->getUser();
-        $results = $musicianAnnounceRepository->findByCriteria($musicianModel, $user);
-        $serializedData = $this->musicianSearchArraySerializer->listToArray($results);
-
-        return $this->json(['data' => $serializedData]);
     }
 }
