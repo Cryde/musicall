1b7ac7777e6159e1684e777de7ed505ce0a857b8
Jérémy <t.jeremy.0@gmail.com>
2024-01-07 17:31:40 +0100

    feat: implement reset password with new brevo provider

--- src/Service/Mail/Brevo/User/ResetPasswordEmail.php
+++ src/Service/Mail/Brevo/User/ResetPasswordEmail.php
@@ -1,29 +1,28 @@
 <?php
 
 namespace App\Service\Mail\Brevo\User;
-
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\Mime\Email;
 
-class ConfirmRegistrationEmail
+class ResetPasswordEmail
 {
-    private const TEMPLATE_ID = '1';
+    private const TEMPLATE_ID = '2';
 
     public function __construct(private readonly MailerInterface $mailer)
     {
     }
 
-    public function send(string $recipientEmail, string $username, string $confirmEmail)
+    public function send(string $recipientEmail, string $username, string $changePasswordLink)
     {
         $email = (new Email())
             ->from(new Address('no-reply@musicall.com', 'MusicAll'))
             ->to(new Address($recipientEmail, $username))
-            ->text('Confirmer votre email');
+            ->text('Réinitialisation du mot de passe');
         $email->getHeaders()
             ->addTextHeader('templateId', self::TEMPLATE_ID)
             ->addParameterizedHeader('params', 'params', [
-                'confirmation_link' => $confirmEmail,
+                'change_password_link' => $changePasswordLink,
                 'username'          => $username,
             ]);
         $this->mailer->send($email);
