966e6a617bb74fdac72e5ecd9e0dde4870fbaa11
Jérémy <t.jeremy.0@gmail.com>
2024-03-03 18:05:21 +0100

    fix: fix property after lexer upgrade

--- src/Extensions/Doctrine/STGeomFromText.php
+++ src/Extensions/Doctrine/STGeomFromText.php
@@ -20,7 +20,7 @@ class STGeomFromText extends FunctionNode
 
         $lexer = $parser->getLexer();
 
-        if ($lexer->lookahead['type'] === Lexer::T_COMMA) {
+        if ($lexer->lookahead->type === Lexer::T_COMMA) {
             $parser->match(Lexer::T_COMMA);
             $this->expressions[] = $parser->ArithmeticFactor();
         }
