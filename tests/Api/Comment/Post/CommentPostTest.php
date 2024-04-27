e775a38acf24f813296f523bf4ea98fd91376c35
Jérémy <t.jeremy.0@gmail.com>
2024-04-27 10:44:33 +0200

    feat: remove previous buggy add comment endpoint & add new one with tests

---
+++ tests/Api/Comment/Post/CommentPostTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Api\Comment\Post;
+
+use App\Repository\Comment\CommentRepository;
+use App\Tests\ApiTestAssertionsTrait;
+use App\Tests\ApiTestCase;
+use App\Tests\Factory\Comment\CommentThreadFactory;
+use App\Tests\Factory\User\UserFactory;
+use Symfony\Component\HttpFoundation\Response;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class CommentPostTest extends ApiTestCase
+{
+    use ResetDatabase, Factories;
+    use ApiTestAssertionsTrait;
+
+    public function test_post(): void
+    {
+        $user1 = UserFactory::new()->asBaseUser()->create(['username' => 'base_user_1', 'email' => 'base_user1@email.com']);
+        $commentThread = CommentThreadFactory::new()->create();
+
+        $user1 = $user1->object();
+        $commentThread = $commentThread->object();
+
+        $this->client->loginUser($user1);
+        $this->client->jsonRequest('POST', '/api/comments', [
+            'thread'  => '/api/comment_threads/' . $commentThread->getId(),
+            'content' => "This is a comment
+with multiline",
+        ], ['CONTENT_TYPE' => 'application/ld+json', 'HTTP_ACCEPT' => 'application/ld+json']);
+
+        $commentRepo = static::getContainer()->get(CommentRepository::class);
+        $comments = $commentRepo->findAll();
+        $this->assertCount(1, $comments);
+        $this->assertResponseStatusCodeSame(Response::HTTP_CREATED);
+        $this->assertJsonEquals([
+            'id' => $comments[0]->getId(),
+            'author' => ['username' => 'base_user_1'],
+            'creation_datetime' => $comments[0]->getCreationDatetime()->format('c'),
+            'content' => "This is a comment<br />\nwith multiline",
+        ]);
+    }
+
+    public function test_not_logged(): void
+    {
+        $commentThread = CommentThreadFactory::new()->create();
+        $this->client->jsonRequest('POST', '/api/comments', [
+            'thread'  => '/api/comment_threads/' . $commentThread->object()->getId(),
+            'content' => 'content',
+        ], ['CONTENT_TYPE' => 'application/ld+json', 'HTTP_ACCEPT' => 'application/ld+json']);
+        $this->assertResponseStatusCodeSame(Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function test_with_invalid_value(): void
+    {
+        $commentThread = CommentThreadFactory::new()->create();
+        $this->client->jsonRequest('POST', '/api/comments', [
+            'thread'  => '/api/comment_threads/' . $commentThread->object()->getId(),
+            'content' => '',
+        ], ['CONTENT_TYPE' => 'application/ld+json', 'HTTP_ACCEPT' => 'application/ld+json']);
+        $this->assertResponseStatusCodeSame(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $this->assertJsonEquals([
+            'status' => 422,
+            'violations' => [
+                [
+                    'propertyPath' => 'content',
+                    'message' => 'Le commentaire est vide',
+                    'code' => 'c1051bb4-d103-4f74-8988-acbcafc7fdc3',
+                ]
+            ],
+            'detail' => 'content: Le commentaire est vide',
+            'hydra:title' => 'An error occurred',
+            'hydra:description' => 'content: Le commentaire est vide',
+            'type' => '/validation_errors/c1051bb4-d103-4f74-8988-acbcafc7fdc3',
+            'title' => 'An error occurred'
+        ]);
+    }
+}
