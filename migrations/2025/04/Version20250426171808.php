7b676861b2a49392b5896bccaf2269d25cf896c5
Cryde <t.jeremy.0@gmail.com>
2025-04-26 19:19:56 +0200

    chore: upgrade to phpstan v2 & fix some type error

---
+++ migrations/2025/04/Version20250426171808.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250426171808 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE message_thread CHANGE last_message_id last_message_id CHAR(36) NOT NULL COMMENT '(DC2Type:uuid)'
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE message_thread CHANGE last_message_id last_message_id CHAR(36) DEFAULT NULL COMMENT '(DC2Type:uuid)'
+        SQL);
+    }
+}
