22b79ef4337050f977a61e2c783e970425d08947
Cryde <t.jeremy.0@gmail.com>
2025-05-02 21:58:42 +0200

    feat: allow user to remove announce

---
+++ src/ApiResource/Musician/Announce.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\ApiResource\Musician;
+
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\OpenApi\Model\Operation;
+use App\State\Processor\Musician\AnnounceDeleteProcessor;
+use App\State\Provider\Musician\AnnounceDeleteProvider;
+
+#[Delete(
+    uriTemplate: '/user/musician/announces/{id}',
+    openapi: new Operation(tags: ['Musician announce']),
+    name: 'api_musician_announces_delete',
+    provider: AnnounceDeleteProvider::class,
+    processor: AnnounceDeleteProcessor::class,
+)]
+class Announce
+{
+}
