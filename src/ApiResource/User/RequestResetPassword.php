5cb59f3e9204d1d9c5284b56062218198b02e579
Cryde <t.jeremy.0@gmail.com>
2025-05-17 15:48:15 +0200

    refactor: use readonly on RequestResetPasswordProcessor & document RequestResetPassword resource

--- src/ApiResource/User/RequestResetPassword.php
+++ src/ApiResource/User/RequestResetPassword.php
@@ -3,11 +3,13 @@
 namespace App\ApiResource\User;
 
 use ApiPlatform\Metadata\Post;
+use ApiPlatform\OpenApi\Model\Operation;
 use App\State\Processor\User\RequestResetPasswordProcessor;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[Post(
     uriTemplate: '/users/request-reset-password',
+    openapi: new Operation(tags: ['Users']),
     name: 'api_user_request_reset_password',
     processor: RequestResetPasswordProcessor::class,
 )]
