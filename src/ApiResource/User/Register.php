d137f4c1f2b8a9bfe6b30a18798826e22d632158
Jérémy <jeremy.tonneau@stellar.io>
2025-05-18 11:25:46 +0200

    refactor: implement register with apip & add more guard to username

---
+++ src/ApiResource/User/Register.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\ApiResource\User;
+
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\OpenApi\Model\Operation;
+use App\Entity\User;
+use App\State\Processor\User\RegisterProcessor;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[Post(
+    uriTemplate: '/users/register',
+    openapi: new Operation(tags: ['Users']),
+    name: 'api_users_register',
+    processor: RegisterProcessor::class,
+)]
+#[UniqueEntity(fields: ['username'], message: 'Ce nom d\'utilisateur est déjà pris', entityClass: User::class)]
+#[UniqueEntity(fields: ['email'], message: 'Cet email est déjà utilisé', entityClass: User::class)]
+class Register
+{
+    #[Assert\NotBlank(message: 'Veuillez saisir un nom d\'utilisateur')]
+    #[Assert\Regex(pattern: '/^[a-zA-Z0-9\._]+$/', message: 'Nom d\'utilisateur invalide : seuls les lettres, chiffres, points et underscores sont autorisés.')]
+    #[Assert\Length(min: 3, max: 40, minMessage: 'Le nom d\'utilisateur doit au moins contenir 3 caractères', maxMessage: 'Le nom d\'utilisateur doit contenir maximum 40 caractères')]
+    public string $username;
+    #[Assert\NotBlank(message: 'Veuillez saisir un email')]
+    #[Assert\Email(message: 'Email invalide')]
+    public string $email;
+    #[Assert\NotBlank(message: 'Veuillez saisir un mot de passe')]
+    #[Assert\Length(min: 6, minMessage: 'Le mot de passe doit au moins contenir 6 caractères')]
+    public string $password;
+}
---
+++ src/State/Processor/User/RegisterProcessor.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\State\Processor\User;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use App\ApiResource\User\Register;
+use App\ApiResource\User\ResetPassword;
+use App\Entity\User;
+use App\Event\UserRegisteredEvent;
+use App\Exception\User\ResetPasswordInvalidTokenException;
+use App\Exception\User\UserAlreadyLoggedException;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Bundle\SecurityBundle\SecurityBundle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+readonly class RegisterProcessor implements ProcessorInterface
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $userPasswordHasher,
+        private Security $security,
+        private EventDispatcherInterface $eventDispatcher,
+    ) {
+    }
+
+    /**
+     * @param Register $data
+     *
+     * @throws UserAlreadyLoggedException
+     */
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
+    {
+        if ($this->security->getUser()) {
+            throw new UserAlreadyLoggedException('Vous êtes déjà connecté');
+        }
+
+        $user = new User()
+            ->setUsername($data->username)
+            ->setEmail($data->email)
+            ->setPlainPassword($data->password);
+
+        $user->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user,
+                $data->password
+            )
+        );
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+        $this->eventDispatcher->dispatch(new UserRegisteredEvent($user));
+    }
+}
