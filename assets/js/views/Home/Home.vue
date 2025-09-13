860106fbde433957ca5d64fa0ecfd9f365d32e3c
Cryde <t.jeremy.0@gmail.com>
2025-09-13 10:50:04 +0200

    fix: improve & fix some issue in the displaying of items & add breadcrumbs

--- assets/js/views/Home/Home.vue
+++ assets/js/views/Home/Home.vue
@@ -1,19 +1,17 @@
 <template>
 
-  <div class="flex md:items-center justify-between gap-1 md:flex-row flex-col">
-    <div class="flex flex-col gap-2">
-      <h2 class="text-2xl font-semibold leading-tight text-surface-900 dark:text-surface-0"> Dernières publications</h2>
-    </div>
-  </div>
-
   <div class="flex flex-row">
     <div class="basis-3/4">
+      <div class="flex justify-between mb-10">
+        <h2 class="text-2xl font-semibold leading-tight text-surface-900 dark:text-surface-0">Dernières
+          publications</h2>
 
-      <div class="flex flex-wrap justify-end-safe gap-4 mb-10">
-        <Button label="Poster une découverte" icon="pi pi-plus" severity="info" size="small"
-                class="whitespace-nowrap"/>
-        <Button label="Poster une publication" icon="pi pi-plus" severity="info" size="small"
-                class="whitespace-nowrap"/>
+        <div class="flex flex-wrap justify-end-safe gap-4">
+          <Button label="Poster une découverte" icon="pi pi-plus" severity="info" size="small"
+                  class="whitespace-nowrap"/>
+          <Button label="Poster une publication" icon="pi pi-plus" severity="info" size="small"
+                  class="whitespace-nowrap"/>
+        </div>
       </div>
 
       <div class="self-stretch flex flex-col gap-8">
@@ -41,7 +39,6 @@ import PublicationListItem from "../Publication/PublicationListItem.vue";
 
 const publicationsStore = usePublicationsStore();
 publicationsStore.loadPublications({page: 1})
-publicationsStore.loadCategories()
 
 onUnmounted(() => {
   publicationsStore.clear();
