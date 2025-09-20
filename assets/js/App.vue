1770de035f2f348c0bb1463ad4592bf7f3dbba33
Cryde <t.jeremy.0@gmail.com>
2025-09-20 17:54:07 +0200

    feat: improve dark mode / light mode on menu

--- assets/js/App.vue
+++ assets/js/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative min-h-[50rem] bg-surface-50 dark:bg-surface-950">
     <Menu />
-    <div class="bg-surface-0 dark:bg-surface-950 px-6 py-8 md:px-12 lg:px-20">
+    <div class="bg-surface-200 dark:bg-surface-950 px-6 py-8 md:px-12 lg:px-20">
       <div class="flex flex-col gap-8">
         <router-view/>
       </div>
