862d622ce07405bacf362500139ad33bf8743181
Jérémy <jeremy.tonneau@stellar.io>
2025-09-21 23:03:12 +0200

    fix: do not use array in Lenght constraint

--- src/ApiResource/Search/UserSearch.php
+++ src/ApiResource/Search/UserSearch.php
@@ -13,7 +13,7 @@ use Symfony\Component\Validator\Constraints\Length;
     name: 'api_users_search',
     provider: UserSearchProvider::class,
     parameters: [
-        'search' => new QueryParameter(key: 'search', description: 'The username you search', constraints: [new Length(['min' => 3])]),
+        'search' => new QueryParameter(key: 'search', description: 'The username you search', constraints: [new Length(min: 3)]),
     ]
 )]
 class UserSearch
