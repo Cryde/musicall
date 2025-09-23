552650b5f5d98dc37b038cdfc4a0d84fd6a22823
Jérémy <jeremy.tonneau@stellar.io>
2025-09-24 00:11:04 +0200

    chore: add comments related to publication

--- src/Fixtures/Publication/PublicationStory.php
+++ src/Fixtures/Publication/PublicationStory.php
@@ -2,6 +2,8 @@
 
 namespace App\Fixtures\Publication;
 
+use App\Fixtures\Factory\Comment\CommentFactory;
+use App\Fixtures\Factory\Comment\CommentThreadFactory;
 use App\Fixtures\Factory\Publication\PublicationFactory;
 use App\Fixtures\User\UserStory;
 use Zenstruck\Foundry\Attribute\AsFixture;
@@ -12,14 +14,24 @@ class PublicationStory extends Story
 {
     public function build(): void
     {
-            PublicationFactory::new()
-                ->with(
-                    static fn() => [
+        PublicationFactory::new()
+            ->with(
+                static function () {
+                    $commentNumber = random_int(0, 5);
+                    $thread = CommentThreadFactory::new(['commentNumber' => $commentNumber])->create();
+                    CommentFactory::new([
+                        'thread' => $thread,
+                        'author' => UserStory::getRandom(UserStory::POOL_USERS),
+                    ])->many($commentNumber)->create();
+
+                    return [
                         'subCategory' => PublicationCategoryStory::getRandom(PublicationCategoryStory::WRITEABLE_CATEGORIES),
-                        'author' => UserStory::getRandom(UserStory::POOL_USERS)
-                    ]
-                )
-                ->many(200)
-                ->applyStateMethod('asBaseTextPublicationOnline')->create();
+                        'author'      => UserStory::getRandom(UserStory::POOL_USERS),
+                        'thread'      => $thread,
+                    ];
+                }
+            )
+            ->many(200)
+            ->applyStateMethod('asBaseTextPublicationOnline')->create();
     }
 }
