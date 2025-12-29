2cdb39509ee9eff9fbbffaf23342c9f326c486e1
Cryde <t.jeremy.0@gmail.com>
2025-12-29 10:37:19 +0100

    fix: use correct openapi tags for users routes

--- src/ApiResource/Search/UserSearch.php
+++ src/ApiResource/Search/UserSearch.php
@@ -4,11 +4,13 @@ namespace App\ApiResource\Search;
 
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\QueryParameter;
+use ApiPlatform\OpenApi\Model\Operation;
 use App\State\Provider\Search\UserSearchProvider;
 use Symfony\Component\Validator\Constraints\Length;
 
 #[GetCollection(
     uriTemplate: '/users/search',
+    openapi: new Operation(tags: ['Users']),
     paginationEnabled: false,
     name: 'api_users_search',
     provider: UserSearchProvider::class,
