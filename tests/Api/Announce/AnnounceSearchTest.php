b28c8b63028b1fa9ab7f27ec60a67f2761270831
Cryde <t.jeremy.0@gmail.com>
2026-01-08 08:38:52 +0100

    tests: fix some broken tests after refactoring the musicians search

--- tests/Api/Announce/AnnounceSearchTest.php
+++ tests/Api/Announce/AnnounceSearchTest.php
@@ -2,27 +2,25 @@
 
 namespace App\Tests\Api\Announce;
 
-use App\Tests\ApiTestAssertionsTrait;
 use App\Tests\ApiTestCase;
 use App\Tests\Factory\Attribute\InstrumentFactory;
 use App\Tests\Factory\Attribute\StyleFactory;
 use App\Tests\Factory\User\MusicianAnnounceFactory;
 use App\Tests\Factory\User\UserFactory;
-use Symfony\Component\HttpFoundation\Response;
 use Zenstruck\Foundry\Test\Factories;
 use Zenstruck\Foundry\Test\ResetDatabase;
 
 class AnnounceSearchTest extends ApiTestCase
 {
     use ResetDatabase, Factories;
-    use ApiTestAssertionsTrait;
 
-    public function test_search_musicians_with_required_parameters(): void
+    public function test_search_musicians_with_filters(): void
     {
         $user = UserFactory::new()->asBaseUser()->create();
         $author1 = UserFactory::new()->asBaseUser()->create(['username' => 'philip', 'email' => 'email']);
         $author2 = UserFactory::new()->asBaseUser()->create(['username' => 'olivia', 'email' => 'olivia@mail.com']);
         $guitar = InstrumentFactory::new()->asGuitar()->create();
+        $drum = InstrumentFactory::new()->asDrum()->create();
         $rock = StyleFactory::new()->asRock()->create();
         $pop = StyleFactory::new()->asPop()->create();
 
@@ -52,6 +50,13 @@ class AnnounceSearchTest extends ApiTestCase
                 'creationDatetime' => new \DateTime('2022-01-01T00:00:00+00:00')
             ]);
 
+        // This announce should NOT be returned (different instrument)
+        MusicianAnnounceFactory::new()
+            ->withInstrument($drum)
+            ->withStyles([$rock])
+            ->asMusician()
+            ->create();
+
         $this->client->loginUser($user->_real());
         $this->client->request(
             'GET',
@@ -63,93 +68,26 @@ class AnnounceSearchTest extends ApiTestCase
         );
 
         $this->assertResponseIsSuccessful();
-        $this->assertJsonEquals([
-            '@context' => '/api/contexts/AnnounceMusician',
-            '@id' => '/api/musicians/search',
-            '@type' => 'Collection',
-            'totalItems' => 2,
-            'member' => [
-                [
-                    '@id' => '/api/announce_musicians/'.$announce2->getId(),
-                    '@type' => 'AnnounceMusician',
-                    'id' => $announce2->getId(),
-                    'location_name' => 'Lyon',
-                    'note' => 'Guitariste rock cherche groupe 2',
-                    'user' => [
-                        '@type' => 'User',
-                        'id' => $author2->getId(),
-                        'username' => 'olivia',
-                    ],
-                    'instrument' => [
-                        '@type' => 'Instrument',
-                        'name' => 'Guitariste'
-                    ],
-                    'type' => 1,
-                    'styles' => [
-                        [
-                            '@type' => 'Style',
-                            'name'  => 'Rock',
-                        ],
-                    ],
-                ],
-                [
-                    '@id' => '/api/announce_musicians/'.$announce1->getId(),
-                    '@type' => 'AnnounceMusician',
-                    'id' => $announce1->getId(),
-                    'location_name' => 'Paris',
-                    'note' => 'Guitariste rock cherche groupe',
-                    'user' => [
-                        '@type' => 'User',
-                        'id' => $author1->getId(),
-                        'username' => 'philip',
-                    ],
-                    'instrument' => [
-                        '@type' => 'Instrument',
-                        'name' => 'Guitariste'
-                    ],
-                    'type' => 1,
-                    'styles' => [
-                        [
-                            '@type' => 'Style',
-                            'name' => 'Rock'
-                        ],
-                        [
-                            '@type' => 'Style',
-                            'name' => 'Pop'
-                        ]
-                    ],
-                ],
-            ],
-            'view' => [
-                '@id' => '/api/musicians/search?instrument=' . $guitar->getId() . '&type=1',
-                '@type' => 'PartialCollectionView'
-            ],
-            'search' => [
-                '@type' => 'IriTemplate',
-                'template' => '/api/musicians/search{?type,instrument,styles}',
-                'variableRepresentation' => 'BasicRepresentation',
-                'mapping' => [
-                    [
-                        '@type' => 'IriTemplateMapping',
-                        'variable' => 'type',
-                        'property' => 'type',
-                        'required' => true,
-                    ],
-                    [
-                        '@type' => 'IriTemplateMapping',
-                        'variable' => 'instrument',
-                        'property' => 'instrument',
-                        'required' => true
-                    ],
-                    [
-                        '@type' => 'IriTemplateMapping',
-                        'variable' => 'styles',
-                        'property' => 'styles',
-                        'required' => false
-                    ],
-                ]
-            ],
-        ]);
+
+        $response = json_decode($this->client->getResponse()->getContent(), true);
+
+        $this->assertEquals('/api/contexts/AnnounceMusician', $response['@context']);
+        $this->assertEquals('/api/musicians/search', $response['@id']);
+        $this->assertEquals('Collection', $response['@type']);
+
+        // Verify only 2 members returned (the guitar ones)
+        $this->assertCount(2, $response['member']);
+
+        // First result (newest) should be Lyon
+        $this->assertEquals($announce2->getId(), $response['member'][0]['id']);
+        $this->assertEquals('Lyon', $response['member'][0]['location_name']);
+        $this->assertEquals('olivia', $response['member'][0]['user']['username']);
+        $this->assertEquals('Guitariste', $response['member'][0]['instrument']['name']);
+
+        // Second result should be Paris
+        $this->assertEquals($announce1->getId(), $response['member'][1]['id']);
+        $this->assertEquals('Paris', $response['member'][1]['location_name']);
+        $this->assertEquals('philip', $response['member'][1]['user']['username']);
     }
 
     public function test_search_musicians_with_latitude_longitude(): void
@@ -160,7 +98,7 @@ class AnnounceSearchTest extends ApiTestCase
         $drum = InstrumentFactory::new()->asDrum()->create();
         $metal = StyleFactory::new()->asMetal()->create();
 
-        $announce1 = MusicianAnnounceFactory::new()
+        MusicianAnnounceFactory::new()
             ->withInstrument($drum)
             ->withStyles([$metal])
             ->asMusician()
@@ -173,7 +111,7 @@ class AnnounceSearchTest extends ApiTestCase
                 'creationDatetime' => new \DateTime('2020-01-01T00:00:00+00:00')
             ]);
 
-        $announce2 = MusicianAnnounceFactory::new()
+        MusicianAnnounceFactory::new()
             ->withInstrument($drum)
             ->withStyles([$metal])
             ->asMusician()
@@ -199,97 +137,26 @@ class AnnounceSearchTest extends ApiTestCase
         );
 
         $this->assertResponseIsSuccessful();
-        $this->assertJsonEquals([
-            '@context' => '/api/contexts/AnnounceMusician',
-            '@id' => '/api/musicians/search',
-            '@type' => 'Collection',
-            'totalItems' => 2,
-            'member' => [
-                [
-                    '@id' => '/api/announce_musicians/'.$announce1->getId(),
-                    '@type' => 'AnnounceMusician',
-                    'id' => $announce1->getId(),
-                    'location_name' => 'Paris',
-                    'note' => 'Batteur metal à Paris',
-                    'user' => [
-                        '@type' => 'User',
-                        'id' => $author1->getId(),
-                        'username' => 'drummer1',
-                    ],
-                    'instrument' => [
-                        '@type' => 'Instrument',
-                        'name' => 'Batteur'
-                    ],
-                    'type' => 1,
-                    'styles' => [
-                        [
-                            '@type' => 'Style',
-                            'name' => 'Metal'
-                        ],
-                    ],
-                    'distance' => 0.0
-                ],
-                [
-                    '@id' => '/api/announce_musicians/'.$announce2->getId(),
-                    '@type' => 'AnnounceMusician',
-                    'id' => $announce2->getId(),
-                    'location_name' => 'Marseille',
-                    'note' => 'Batteur metal à Marseille',
-                    'user' => [
-                        '@type' => 'User',
-                        'id' => $author2->getId(),
-                        'username' => 'drummer2',
-                    ],
-                    'instrument' => [
-                        '@type' => 'Instrument',
-                        'name' => 'Batteur'
-                    ],
-                    'type' => 1,
-                    'styles' => [
-                        [
-                            '@type' => 'Style',
-                            'name' => 'Metal'
-                        ],
-                    ],
-                    'distance' => 660.476928300675
-                ],
-            ],
-            'view' => [
-                '@id' => '/api/musicians/search?instrument=' . $drum->getId() . '&latitude=48.8566&longitude=2.3522&type=1',
-                '@type' => 'PartialCollectionView'
-            ],
-            'search' => [
-                '@type' => 'IriTemplate',
-                'template' => '/api/musicians/search{?type,instrument,styles}',
-                'variableRepresentation' => 'BasicRepresentation',
-                'mapping' => [
-                    [
-                        '@type' => 'IriTemplateMapping',
-                        'variable' => 'type',
-                        'property' => 'type',
-                        'required' => true,
-                    ],
-                    [
-                        '@type' => 'IriTemplateMapping',
-                        'variable' => 'instrument',
-                        'property' => 'instrument',
-                        'required' => true
-                    ],
-                    [
-                        '@type' => 'IriTemplateMapping',
-                        'variable' => 'styles',
-                        'property' => 'styles',
-                        'required' => false
-                    ],
-                ]
-            ],
-        ]);
+
+        // With coordinates, results should be ordered by distance (Paris first, then Marseille)
+        $response = json_decode($this->client->getResponse()->getContent(), true);
+        $this->assertCount(2, $response['member']);
+
+        // First result should be Paris (closest to search coordinates)
+        $this->assertEquals('Paris', $response['member'][0]['location_name']);
+        $this->assertArrayHasKey('distance', $response['member'][0]);
+        $this->assertEquals(0.0, $response['member'][0]['distance']);
+
+        // Second result should be Marseille (further away)
+        $this->assertEquals('Marseille', $response['member'][1]['location_name']);
+        $this->assertArrayHasKey('distance', $response['member'][1]);
+        $this->assertGreaterThan(0, $response['member'][1]['distance']);
     }
 
     public function test_search_musicians_with_styles_filter(): void
     {
         $user = UserFactory::new()->asBaseUser()->create();
-        $author2 = UserFactory::new()->asBaseUser()->create(['username' => 'olivia', 'email' => 'olivia@email.com']);
+        $author = UserFactory::new()->asBaseUser()->create(['username' => 'olivia', 'email' => 'olivia@email.com']);
         $guitar = InstrumentFactory::new()->asGuitar()->create();
         $rock = StyleFactory::new()->asRock()->create();
         $pop = StyleFactory::new()->asPop()->create();
@@ -299,8 +166,9 @@ class AnnounceSearchTest extends ApiTestCase
             ->withInstrument($guitar)
             ->withStyles([$rock, $pop])
             ->asMusician()
-            ->create(['author' => $author2, 'note' => 'some notes']);
+            ->create(['author' => $author, 'note' => 'some notes']);
 
+        // This announce should NOT be returned (different style)
         MusicianAnnounceFactory::new()
             ->withInstrument($guitar)
             ->withStyles([$metal])
@@ -318,73 +186,14 @@ class AnnounceSearchTest extends ApiTestCase
             ]
         );
 
-        $this->assertJsonEquals([
-            '@context' => '/api/contexts/AnnounceMusician',
-            '@id' => '/api/musicians/search',
-            '@type' => 'Collection',
-            'totalItems' => 1,
-            'member' => [
-                [
-                    '@id' => '/api/announce_musicians/'.$announce1->getId(),
-                    '@type' => 'AnnounceMusician',
-                    'id' => $announce1->getId(),
-                    'location_name' => $announce1->getLocationName(),
-                    'note' => 'some notes',
-                    'user' => [
-                        '@type' => 'User',
-                        'id' => $author2->getId(),
-                        'username' => 'olivia',
-                    ],
-                    'instrument' => [
-                        '@type' => 'Instrument',
-                        'name' => 'Guitariste'
-                    ],
-                    'type' => 1,
-                    'styles' => [
-                        [
-                            '@type' => 'Style',
-                            'name' => 'Rock'
-                        ],
-                        [
-                            '@type' => 'Style',
-                            'name' => 'Pop'
-                        ]
-                    ],
-                ],
-            ],
-            'view' => [
-                '@id' => '/api/musicians/search?instrument=' . $guitar->getId() . '&styles%5B%5D='.$rock->getId().'&type=1',
-                '@type' => 'PartialCollectionView'
-            ],
-            'search' => [
-                '@type' => 'IriTemplate',
-                'template' => '/api/musicians/search{?type,instrument,styles}',
-                'variableRepresentation' => 'BasicRepresentation',
-                'mapping' => [
-                    [
-                        '@type' => 'IriTemplateMapping',
-                        'variable' => 'type',
-                        'property' => 'type',
-                        'required' => true,
-                    ],
-                    [
-                        '@type' => 'IriTemplateMapping',
-                        'variable' => 'instrument',
-                        'property' => 'instrument',
-                        'required' => true
-                    ],
-                    [
-                        '@type' => 'IriTemplateMapping',
-                        'variable' => 'styles',
-                        'property' => 'styles',
-                        'required' => false
-                    ],
-                ]
-            ],
-        ]);
+        $this->assertResponseIsSuccessful();
+
+        $response = json_decode($this->client->getResponse()->getContent(), true);
+        $this->assertCount(1, $response['member']);
+        $this->assertEquals($announce1->getId(), $response['member'][0]['id']);
     }
 
-    public function test_search_musicians_without_required_type_parameter(): void
+    public function test_search_musicians_without_parameters_returns_success(): void
     {
         $user = UserFactory::new()->asBaseUser()->create();
 
@@ -394,30 +203,6 @@ class AnnounceSearchTest extends ApiTestCase
             '/api/musicians/search',
         );
 
-        $this->assertResponseStatusCodeSame(Response::HTTP_UNPROCESSABLE_ENTITY);
-        $this->assertJsonEquals([
-            '@context' => '/api/contexts/ConstraintViolation',
-            '@id' => '/api/validation_errors/0=ad32d13f-c3d4-423b-909a-857b961eb720;1=ad32d13f-c3d4-423b-909a-857b961eb720',
-            '@type' => 'ConstraintViolation',
-            'status' => 422,
-            'violations' => [
-                [
-                    'propertyPath' => 'type',
-                    'message' => 'Cette valeur ne doit pas être nulle.',
-                    'code' => 'ad32d13f-c3d4-423b-909a-857b961eb720',
-                ],
-                [
-                    'propertyPath' => 'instrument',
-                    'message' => 'Cette valeur ne doit pas être nulle.',
-                    'code' => 'ad32d13f-c3d4-423b-909a-857b961eb720',
-                ],
-            ],
-            'detail' => 'type: Cette valeur ne doit pas être nulle.
-instrument: Cette valeur ne doit pas être nulle.',
-            'description' => 'type: Cette valeur ne doit pas être nulle.
-instrument: Cette valeur ne doit pas être nulle.',
-            'type' => '/validation_errors/0=ad32d13f-c3d4-423b-909a-857b961eb720;1=ad32d13f-c3d4-423b-909a-857b961eb720',
-            'title' => 'An error occurred',
-        ]);
+        $this->assertResponseIsSuccessful();
     }
 }
