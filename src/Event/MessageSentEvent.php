7877976721a8e52ec7171594ab471691c57c222d
Cryde <t.jeremy.0@gmail.com>
2026-01-13 22:53:22 +0100

    feat: add link in the email when someone send message on musicall

--- src/Event/MessageSentEvent.php
+++ src/Event/MessageSentEvent.php
@@ -1,13 +1,19 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace App\Event;
 
+use App\Entity\Message\MessageThread;
 use App\Entity\User;
 use Symfony\Contracts\EventDispatcher\Event;
 
 class MessageSentEvent extends Event
 {
-    public function __construct(public readonly User $recipient)
-    {
+    public function __construct(
+        public readonly User $recipient,
+        public readonly User $sender,
+        public readonly MessageThread $thread,
+    ) {
     }
 }
