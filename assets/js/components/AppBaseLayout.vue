07cadee190d39f55bfe664ce658cdd4600a81067
Cryde <t.jeremy.0@gmail.com>
2026-01-21 09:45:58 +0100

    feat: improve search mobile experience & profile navigation

--- assets/js/components/AppBaseLayout.vue
+++ assets/js/components/AppBaseLayout.vue
@@ -4,7 +4,11 @@
         <Menu />
         <div class="bg-surface-200 dark:bg-surface-950 px-6 py-8 md:px-12 lg:px-20">
             <div class="flex flex-col gap-8">
-                <router-view/>
+                <router-view v-slot="{ Component }">
+                    <KeepAlive :include="keepAliveIncludes" :max="3">
+                        <component :is="Component" />
+                    </KeepAlive>
+                </router-view>
             </div>
         </div>
         <Footer/>
@@ -12,6 +16,30 @@
 </template>
 <script setup>
 import Toast from 'primevue/toast'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 import Footer from '../views/Global/Footer.vue'
 import Menu from '../views/Global/Menu.vue'
+
+const route = useRoute()
+
+// Context zones: routes where specific components should remain cached
+const musicianSearchContext = [
+    'app_search_musician',
+    'app_search_guitarist',
+    'app_search_drummer',
+    'app_search_bassist',
+    'app_search_singer',
+    'app_search_pianist',
+    'app_user_public_profile',
+    'app_user_musician_profile'
+]
+
+// Dynamic include list based on current route context
+const keepAliveIncludes = computed(() => {
+    if (musicianSearchContext.includes(route.name)) {
+        return ['MusicianSearch']
+    }
+    return []
+})
 </script>
